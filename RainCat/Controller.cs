bf2ac5c7fb3d8c5b1127af1f2fb4ab45fa16e03b
user1 <user1@526-6>
2022-11-02 13:44:27 +0400

    Добавьте файлы проекта.

---
+++ RainCat/Controller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RainCat
+{
+    internal class Controller
+    {
+     public void Save(Cat[] cats)
+     {
+            var json = JsonSerializer.Serialize<Cat[]>(cats);
+            File.WriteAllText("cats.json", json);
+     }
+     public Cat[] Load(string path)
+        {
+            path = "cats.json";
+            if (!File.Exists(path))
+            {
+                return null;
+            }
+            else
+            {
+                var json = File.ReadAllText(path);
+                Cat[] cats = JsonSerializer.Deserialize<Cat[]>(json);
+                return cats;
+            }
+        }
+
+    }
+
+    
+}
---
+++ RainCat/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RainCat;
+
+Console.WriteLine("Hello, World!");
+Cat[] cats= new Cat[3];
+Cat persik = new Cat();
+persik.Nick = "Персик";
+persik.Year = 2;
+persik.breed = "Британец";
+Cat pushok = new Cat();
+pushok.Nick = "Пушок";
+pushok.Year = 3;
+pushok.breed = "Немец";
+Cat charlis = new Cat();
+charlis.Nick = "Чарлис";
+charlis.Year = 4;
+charlis.breed = "Сиамец";
+
+cats[0] = persik;
+cats[1] = pushok;
+cats[2] = charlis;
+
+Controller controller = new Controller();
+controller.Save(cats);
+string path = "cats.json";
+var t = controller.Load(path);
+foreach (var item in t)
+{
+    Console.WriteLine(item.Nick,item.Year,item.breed);
+}
